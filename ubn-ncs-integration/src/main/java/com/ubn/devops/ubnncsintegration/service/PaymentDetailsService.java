b13f50cf11843361734cbd281cb2a3a11ef08468
Lawal Babatunde <lababatunde@unionbankng.com>
2019-09-05 12:14:10 +0100

    Signed-off-by: Lawal Babatunde <lababatunde@unionbankng.com>

--- ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/service/PaymentDetailsService.java
+++ ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/service/PaymentDetailsService.java
@@ -2,8 +2,6 @@ package com.ubn.devops.ubnncsintegration.service;
 
 import com.ubn.devops.ubnncsintegration.model.PaymentDetails;
 import com.ubn.devops.ubnncsintegration.ncsschema.EAssessmentNotice;
-import com.ubn.devops.ubnncsintegration.ncsschema.EPaymentConfirmation;
-import com.ubn.devops.ubnncsintegration.ncsschema.TransactionResponse;
 import com.ubn.devops.ubnncsintegration.request.PaymentProcessRequest;
 import com.ubn.devops.ubnncsintegration.response.ApiResponse;
 
@@ -11,10 +9,6 @@ public interface PaymentDetailsService {
 	
 	public PaymentDetails savePaymentDetails(EAssessmentNotice assessmentNotice);
 	
-	public PaymentDetails updatePaymentDetailsWithRequest(EPaymentConfirmation paymentConirmation);
-	
-	public void updatePaymentDetailsWithResponse(TransactionResponse response);
-
 	public ApiResponse fetchPaymentDetails(String declarantCode);
 	
 	public void acknowledgePaymentDetails(String declarantCode);
--- ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/serviceimpl/PaymentDetailsServiceImpl.java
+++ ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/serviceimpl/PaymentDetailsServiceImpl.java
@@ -3,17 +3,12 @@ package com.ubn.devops.ubnncsintegration.serviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ubn.devops.ubnncsintegration.config.FilePathsConfig;
 import com.ubn.devops.ubnncsintegration.model.PaymentDetails;
 import com.ubn.devops.ubnncsintegration.ncsschema.EAssessmentNotice;
-import com.ubn.devops.ubnncsintegration.ncsschema.EPaymentConfirmation;
-import com.ubn.devops.ubnncsintegration.ncsschema.TransactionResponse;
 import com.ubn.devops.ubnncsintegration.repository.PaymentDetailsRepository;
-import com.ubn.devops.ubnncsintegration.repository.TaxEntityRepository;
 import com.ubn.devops.ubnncsintegration.request.PaymentProcessRequest;
 import com.ubn.devops.ubnncsintegration.response.ApiResponse;
 import com.ubn.devops.ubnncsintegration.service.PaymentDetailsService;
-import com.ubn.devops.ubnncsintegration.utility.Utils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,16 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 public class PaymentDetailsServiceImpl implements PaymentDetailsService {
 
 	@Autowired
-	private FilePathsConfig pathsConfig;
-
-	@Autowired
 	private PaymentDetailsRepository paymentRepo;
-
-	@Autowired
-	private TaxEntityRepository taxRepo;
-
-	@Autowired
-	private Utils utils;
 
 	@Override
 	public PaymentDetails savePaymentDetails(EAssessmentNotice assessmentNotice) {
@@ -46,58 +32,32 @@ public class PaymentDetailsServiceImpl implements PaymentDetailsService {
 		return paymentDetails;
 	}
 
-	@Override
-	public void updatePaymentDetailsWithResponse(TransactionResponse response) {
-		try {
-			/*
-			 * String declarantCode = response.getDeclarantCode();
-			 * log.info("====Trying to update payment details with declarant code:" +
-			 * declarantCode + " ======="); try { PaymentDetails paymentModel =
-			 * paymentRepo.findByDeclarantCode(declarantCode); if (paymentModel != null) {
-			 * String rspMessage = "";
-			 * paymentModel.setTransactionstatus(response.getTransactionStatus().value());
-			 * Info info = response.getInfo(); if (info != null) { if
-			 * (!info.getMessage().isEmpty()) { rspMessage =
-			 * StringUtils.collectionToCommaDelimitedString(info.getMessage()); } }
-			 * paymentModel.setResponseMessage(rspMessage); paymentRepo.save(paymentModel);
-			 * log.info("successfully updated the payment object with declarant code: "
-			 * +declarantCode+" with transaction response"); } else {
-			 * 
-			 * log.warn("======The declarant code:" + declarantCode + " does not exist"); }
-			 */
-		} catch (Exception ex) {
-			log.error("error occured while trying to update payment details because: " + ex.getMessage(), ex);
-		}
-	}
-
-	@Override
-	public PaymentDetails updatePaymentDetailsWithRequest(EPaymentConfirmation paymentConfirmation) {
-		PaymentDetails entity = null;
-		String declarantCode = paymentConfirmation.getDeclarantCode();
-		log.info("=======Trying to update payment details with request using declarant code:" + declarantCode
-				+ "======");
-		try {
-			/*
-			 * PaymentDetails model = paymentRepo.findByDeclarantCode(declarantCode); if
-			 * (model != null) { Payment payment = paymentConfirmation.getPayment().get(0);
-			 * if (payment != null) { model.setReference(payment.getReference());
-			 * model.setAmount(payment.getAmount());
-			 * model.setMeansOfPayment(payment.getMeansOfPayment()); } entity =
-			 * paymentRepo.save(model); CustomMarshaller.marshall(paymentConfirmation,
-			 * pathsConfig.getPaymentrequest());
-			 * log.info("Successfully updated payment details with declarant code: "
-			 * +declarantCode+" with request"); } else {
-			 * log.warn("No declarant code: "+declarantCode+" was found"); }
-			 */
-		} catch (Exception ex) {
-			log.error("Error occured while saving payment details because: " + ex.getMessage(), ex);
-		}
-		return entity;
-	}
-
+	/*
+	 * @Override public void updatePaymentDetailsWithResponse(TransactionResponse
+	 * response) { try {
+	 * 
+	 * String declarantCode = response.getDeclarantCode();
+	 * log.info("====Trying to update payment details with declarant code:" +
+	 * declarantCode + " ======="); try { PaymentDetails paymentModel =
+	 * paymentRepo.findByDeclarantCode(declarantCode); if (paymentModel != null) {
+	 * String rspMessage = "";
+	 * paymentModel.setTransactionstatus(response.getTransactionStatus().value());
+	 * Info info = response.getInfo(); if (info != null) { if
+	 * (!info.getMessage().isEmpty()) { rspMessage =
+	 * StringUtils.collectionToCommaDelimitedString(info.getMessage()); } }
+	 * paymentModel.setResponseMessage(rspMessage); paymentRepo.save(paymentModel);
+	 * log.info("successfully updated the payment object with declarant code: "
+	 * +declarantCode+" with transaction response"); } else {
+	 * 
+	 * log.warn("======The declarant code:" + declarantCode + " does not exist"); }
+	 * 
+	 * } catch (Exception ex) {
+	 * log.error("error occured while trying to update payment details because: " +
+	 * ex.getMessage(), ex); } }
+	 */
 	@Override
 	public ApiResponse fetchPaymentDetails(String declarantCode) {
-	ApiResponse response = new ApiResponse(ApiResponse.SERVER_ERROR,
+		ApiResponse response = new ApiResponse(ApiResponse.SERVER_ERROR,
 				"Error occured while fetching details by declarant code.");
 		log.info("======Trying to fetch payment details using declarantcode: " + declarantCode + "======");
 		try {
@@ -127,25 +87,24 @@ public class PaymentDetailsServiceImpl implements PaymentDetailsService {
 
 	@Override
 	public ApiResponse processPayment(PaymentProcessRequest paymentProcessRequest) {
-		log.info("processing payment request with details: "+paymentProcessRequest.toString());
-		ApiResponse response = new ApiResponse(ApiResponse.SERVER_ERROR,"Unable to process request. Please try again");
+		log.info("processing payment request with details: " + paymentProcessRequest.toString());
+		ApiResponse response = new ApiResponse(ApiResponse.SERVER_ERROR, "Unable to process request. Please try again");
 		try {
 			PaymentDetails details = paymentRepo.findPaymentDetails(paymentProcessRequest.getDeclarantCode());
-			if(details!=null) {
-				if(details.isPaid()) {
+			if (details != null) {
+				if (details.isPaid()) {
 					response.setCode(ApiResponse.ALREADY_PAID);
 					response.setMessage("This payment has already been made");
-				}else {
-					
+				} else {
+
 				}
-			}else {
+			} else {
 				response.setCode(ApiResponse.NOT_FOUND);
 				response.setMessage("The payment with this details does not exist");
 			}
-			
-			
-		}catch(Exception ex) {
-			log.error("error occured while processing payment request because: "+ex.getMessage(),ex);
+
+		} catch (Exception ex) {
+			log.error("error occured while processing payment request because: " + ex.getMessage(), ex);
 		}
 		return response;
 	}
