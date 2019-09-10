086422c5283928479766cdd036b5bdd7deaf2713
Lawal Babatunde <lababatunde@unionbankng.com>
2019-09-10 16:24:24 +0100

    Signed-off-by: Lawal Babatunde <lababatunde@unionbankng.com>

--- ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/repository/TaxEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ubn.devops.ubnncsintegration.repository;
-
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaxEntityRepository {//extends JpaRepository<TaxEntity,Long> {
-     
-}
