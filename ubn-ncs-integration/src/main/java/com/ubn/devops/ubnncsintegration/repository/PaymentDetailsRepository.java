1da63fbc9377c9696ee25500942aa92fc6d5f715
aojinadu <aojinadu@DEVOPS-22.unionbank-ng.unionbankgroup.local>
2019-09-16 16:23:58 +0100

    Done with NCS Integration for sweep

--- ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/repository/PaymentDetailsRepository.java
+++ ubn-ncs-integration/src/main/java/com/ubn/devops/ubnncsintegration/repository/PaymentDetailsRepository.java
@@ -1,7 +1,6 @@
 package com.ubn.devops.ubnncsintegration.repository;
 
 import java.math.BigDecimal;
-import java.sql.ResultSet;
 import java.sql.Types;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,12 +8,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -36,9 +39,8 @@ public class PaymentDetailsRepository {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	private String SCHEMANAME = PropsReader.getValue("db.schemaname");
-	
-	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
 	private final String PACKAGENAME = PropsReader.getValue("db.packagename");
 
@@ -65,15 +67,13 @@ public class PaymentDetailsRepository {
 						new SqlParameter("P_SADASSESSMENTNUMBER", Types.VARCHAR),
 						new SqlParameter("P_SADASSESSMENTDATE", Types.DATE),
 						new SqlParameter("P_COMPANYCODE", Types.VARCHAR),
-						new SqlParameter("P_COMPANYNAME", Types.VARCHAR), 
-						new SqlParameter("BANKCODE", Types.VARCHAR),
+						new SqlParameter("P_COMPANYNAME", Types.VARCHAR), new SqlParameter("BANKCODE", Types.VARCHAR),
 						new SqlParameter("BANKBRANCHCODE", Types.VARCHAR),
 						new SqlParameter("P_FORMMNUMBER", Types.VARCHAR),
 						new SqlParameter("P_TOTALAMOUNTTOBEPAID", Types.DECIMAL),
 						new SqlOutParameter("P_RESPONSECODE", Types.VARCHAR),
-						new SqlOutParameter("P_RESPONSEMSG", Types.VARCHAR),
-						new SqlOutParameter("P_ID", Types.DECIMAL),
-						new SqlParameter("P_VERSION",Types.VARCHAR));
+						new SqlOutParameter("P_RESPONSEMSG", Types.VARCHAR), new SqlOutParameter("P_ID", Types.DECIMAL),
+						new SqlParameter("P_VERSION", Types.VARCHAR));
 				Map<String, Object> paramSource = new HashMap<>();
 				paramSource.put("P_SADYEAR", payment.getSadYear());
 				paramSource.put("P_CUSTOMSCODE", payment.getCustomsCode());
@@ -96,7 +96,7 @@ public class PaymentDetailsRepository {
 						log.info("Successfully saved the payment details with declarant code: "
 								+ assessmentNotice.getDeclarantCode());
 						payment.setId(((BigDecimal) respValues.get("P_ID")).longValue());
-						for(TaxEntity tax:payment.getTaxes()) {
+						for (TaxEntity tax : payment.getTaxes()) {
 							tax.setPaymentDetailsId(payment.getId());
 							saveTax(tax);
 						}
@@ -187,7 +187,7 @@ public class PaymentDetailsRepository {
 		return model;
 	}
 
-	public int validateTransactionReference(String txnReference,String formMNumber) {
+	public int validateTransactionReference(String txnReference, String formMNumber) {
 		int responseValue = 0;
 
 		try {
@@ -201,8 +201,7 @@ public class PaymentDetailsRepository {
 					new SqlOutParameter("p_responsecode", Types.VARCHAR));
 			Map<String, Object> paramSource = new HashMap<>();
 			paramSource.put("p_formmnumber", formMNumber);
-			paramSource.put("p_txnreference"
-					, txnReference);
+			paramSource.put("p_txnreference", txnReference);
 			Map<String, Object> result = call.execute(paramSource);
 			if (!result.isEmpty()) {
 				String rspCode = result.get("p_responsecode").toString();
@@ -225,48 +224,46 @@ public class PaymentDetailsRepository {
 
 	public int updateSuccessfulValidation(PaymentProcessRequest request) {
 		int isUpdated = 0;
-	
-			log.info("Trying to update payment details After successful FCUBS ref validation with formMNumber:" + request.getFormMNumber() + " as acknowledge");
-			try {
-				SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate);
-				call.setSchemaName(SCHEMANAME);
-				call.setCatalogName(PACKAGENAME);
-				call.setProcedureName("UPDATESUCCESSFULVALIDATION");
-				call.declareParameters(new SqlParameter("P_FORMMNUMBER", Types.VARCHAR),
-						new SqlParameter("P_TXNREF", Types.VARCHAR),
-						new SqlParameter("P_ACCTNO", Types.VARCHAR),
-						new SqlParameter("P_CUSTEMAIL", Types.VARCHAR),
-						new SqlParameter("P_AMOUNTPAID", Types.DECIMAL),
-						new SqlParameter("P_CHANNEL", Types.VARCHAR),
-						new SqlParameter("P_PAYMENTDATE", Types.TIMESTAMP),
-						new SqlOutParameter("P_RESPONSEMSG", Types.VARCHAR),
-						new SqlOutParameter("P_RESPONSECODE", Types.VARCHAR));
-				Map<String, Object> paramSource = new HashMap<>();
-				paramSource.put("P_FORMMNUMBER", request.getFormMNumber());
-				paramSource.put("P_TXNREF", request.getExternalRef());
-				paramSource.put("P_ACCTNO", request.getAccount());
-				paramSource.put("P_CUSTEMAIL", request.getCustomerEmail());
-				paramSource.put("P_AMOUNTPAID", new BigDecimal(request.getAmount()));
-				paramSource.put("P_CHANNEL", request.getChannel());
-				paramSource.put("P_PAYMENTDATE", sdf.format(request.getPostingDate()));
-				Map<String, Object> respValues = call.execute(paramSource);
-				if (!respValues.isEmpty()) {
-					String responsecode = respValues.get("P_RESPONSECODE").toString();
-					if (responsecode.equals("00")) {
-						isUpdated =1;
-						log.info("Successfully updated the payment details with formMNumber: " + request.getFormMNumber()
-								+ " as acknowledge");
 
-					} else if (responsecode.equals("99")) {
-						log.warn("db error occured with message: " + respValues.get("P_RESPONSEMSG"));
-					}
+		log.info("Trying to update payment details After successful FCUBS ref validation with formMNumber:"
+				+ request.getFormMNumber() + " as acknowledge");
+		try {
+			SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate);
+			call.setSchemaName(SCHEMANAME);
+			call.setCatalogName(PACKAGENAME);
+			call.setProcedureName("UPDATESUCCESSFULVALIDATION");
+			call.declareParameters(new SqlParameter("P_FORMMNUMBER", Types.VARCHAR),
+					new SqlParameter("P_TXNREF", Types.VARCHAR), new SqlParameter("P_ACCTNO", Types.VARCHAR),
+					new SqlParameter("P_CUSTEMAIL", Types.VARCHAR), new SqlParameter("P_AMOUNTPAID", Types.DECIMAL),
+					new SqlParameter("P_CHANNEL", Types.VARCHAR), new SqlParameter("P_PAYMENTDATE", Types.TIMESTAMP),
+					new SqlOutParameter("P_RESPONSEMSG", Types.VARCHAR),
+					new SqlOutParameter("P_RESPONSECODE", Types.VARCHAR));
+			Map<String, Object> paramSource = new HashMap<>();
+			paramSource.put("P_FORMMNUMBER", request.getFormMNumber());
+			paramSource.put("P_TXNREF", request.getExternalRef());
+			paramSource.put("P_ACCTNO", request.getAccount());
+			paramSource.put("P_CUSTEMAIL", request.getCustomerEmail());
+			paramSource.put("P_AMOUNTPAID", new BigDecimal(request.getAmount()));
+			paramSource.put("P_CHANNEL", request.getChannel());
+			paramSource.put("P_PAYMENTDATE", sdf.format(request.getPostingDate()));
+			Map<String, Object> respValues = call.execute(paramSource);
+			if (!respValues.isEmpty()) {
+				String responsecode = respValues.get("P_RESPONSECODE").toString();
+				if (responsecode.equals("00")) {
+					isUpdated = 1;
+					log.info("Successfully updated the payment details with formMNumber: " + request.getFormMNumber()
+							+ " as acknowledge");
+
+				} else if (responsecode.equals("99")) {
+					log.warn("db error occured with message: " + respValues.get("P_RESPONSEMSG"));
 				}
+			}
 		} catch (Exception ex) {
-			log.info("error while updating details with payment info because: "+ex.getMessage(),ex);
+			log.info("error while updating details with payment info because: " + ex.getMessage(), ex);
 		}
 		return isUpdated;
 	}
-	
+
 	public int updatePaymentWithNCSResponse(TransactionResponse response) {
 		int isUpdated = 0;
 		log.info("trying to update payment details with ");
@@ -279,13 +276,12 @@ public class PaymentDetailsRepository {
 					new SqlParameter("P_CUSTOMSCODE", Types.VARCHAR),
 					new SqlParameter("P_SADASSESSMENTSERIAL", Types.VARCHAR),
 					new SqlParameter("P_SADASSESSMENTNUMBER", Types.VARCHAR),
-					new SqlParameter("P_VERSION", Types.VARCHAR),
-					new SqlParameter("P_NCS_RESPCODE", Types.VARCHAR),
+					new SqlParameter("P_VERSION", Types.VARCHAR), new SqlParameter("P_NCS_RESPCODE", Types.VARCHAR),
 					new SqlParameter("P_NCS_RESPMSG", Types.VARCHAR),
 					new SqlOutParameter("P_RESPONSECODE", Types.VARCHAR),
 					new SqlOutParameter("P_RESPONSEMSG", Types.DECIMAL)
-					
-					);
+
+			);
 			Map<String, Object> paramSource = new HashMap<>();
 			paramSource.put("P_SADYEAR", response.getSadAsmt().getSadYear());
 			paramSource.put("P_CUSTOMSCODE", response.getCustomsCode());
@@ -293,26 +289,27 @@ public class PaymentDetailsRepository {
 			paramSource.put("P_SADASSESSMENTNUMBER", response.getSadAsmt().getSadAssessmentNumber());
 			paramSource.put("P_VERSION", response.getSadAsmt().getSadYear());
 			paramSource.put("P_NCS_RESPCODE", response.getTransactionStatus().value());
-			paramSource.put("P_NCS_RESPMSG", StringUtils.collectionToDelimitedString(response.getInfo().getMessage(),","));
+			paramSource.put("P_NCS_RESPMSG",
+					StringUtils.collectionToDelimitedString(response.getInfo().getMessage(), ","));
 			Map<String, Object> respValues = call.execute(paramSource);
-			if(!respValues.isEmpty()) {
+			if (!respValues.isEmpty()) {
 				String code = respValues.get("P_RESPONSECODE").toString();
-				if(code.equals("00")) {
-					isUpdated=1;
+				if (code.equals("00")) {
+					isUpdated = 1;
 					log.info("successfully updated payment with NCS response");
-				}else {
+				} else {
 					String errorMessage = respValues.get("P_RESPONSEMSG").toString();
-					log.warn("db error occured with error message: "+errorMessage);
+					log.warn("db error occured with error message: " + errorMessage);
 				}
-			}				
-		}catch(Exception ex) {
-			log.error("Error occured while trying to update payment with NCS response because: "+ex.getMessage(),ex);
+			}
+		} catch (Exception ex) {
+			log.error("Error occured while trying to update payment with NCS response because: " + ex.getMessage(), ex);
 		}
 		return isUpdated;
 	}
-	
+
 	public int saveTax(TaxEntity tax) {
-		int isSaved=0;
+		int isSaved = 0;
 		log.info("trying to save tax entity");
 		try {
 			SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate);
@@ -320,8 +317,7 @@ public class PaymentDetailsRepository {
 			call.setCatalogName(PACKAGENAME);
 			call.setProcedureName("SAVETAX");
 			call.declareParameters(new SqlParameter("P_TAXCODE", Types.VARCHAR),
-					new SqlParameter("P_TAXAMOUNT", Types.VARCHAR),
-					new SqlParameter("P_PAYMENTID", Types.NUMERIC),
+					new SqlParameter("P_TAXAMOUNT", Types.VARCHAR), new SqlParameter("P_PAYMENTID", Types.NUMERIC),
 					new SqlOutParameter("P_RESPONSECODE", Types.VARCHAR),
 					new SqlOutParameter("P_RESPONSEMSG", Types.VARCHAR));
 			Map<String, Object> paramSource = new HashMap<>();
@@ -329,55 +325,58 @@ public class PaymentDetailsRepository {
 			paramSource.put("P_TAXAMOUNT", tax.getTaxAmount());
 			paramSource.put("P_PAYMENTID", tax.getPaymentDetailsId());
 			Map<String, Object> respValues = call.execute(paramSource);
-			if(!respValues.isEmpty()) {
+			if (!respValues.isEmpty()) {
 				String rspCode = respValues.get("P_RESPONSECODE").toString();
-				if(rspCode.equals("00")) {
+				if (rspCode.equals("00")) {
 					isSaved = 1;
 					log.info("successfully saved tax entity");
-				}else {
-					log.warn("db error occured while trying to save tax entity with error message: "+respValues.get("P_RESPONSEMSG").toString());
+				} else {
+					log.warn("db error occured while trying to save tax entity with error message: "
+							+ respValues.get("P_RESPONSEMSG").toString());
 				}
 			}
-		}catch(Exception ex) {
-			log.error("Error occured while trying to save tax entity because: "+ex.getMessage(),ex);
+		} catch (Exception ex) {
+			log.error("Error occured while trying to save tax entity because: " + ex.getMessage(), ex);
 		}
 		return isSaved;
 	}
-	
+
 	public List<SweepPaymentDetails> findFCUBSDetails(String reference, String code) {
 		List<SweepPaymentDetails> models = new ArrayList<>();
 		SweepPaymentDetails model = null;
-		ResultSet rs = null;
+//		ResultSet rs = null;
 		try {
 			SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate);
 			call.setSchemaName(SCHEMANAME);
 			call.setCatalogName(PACKAGENAME);
-			call.setProcedureName("GETSWEEPPAYMENTDETAILS");
-			call.declareParameters(new SqlOutParameter("RESPONSE", Types.VARCHAR),
-					new SqlOutParameter("PAYMENTDETAILS", Types.REF_CURSOR));
-			Map<String, Object> paramSource = new HashMap<>();
-			paramSource.put("paymentref", reference);
-			paramSource.put("task_code", code);
+			call.withFunctionName("GETSWEEPPAYMENTDETAILS");
+//			call.declareParameters(new SqlOutParameter("PAYMENTDETAILS", Types.REF_CURSOR));
+			SqlParameterSource paramSource = new MapSqlParameterSource().addValue("paymentref", reference)
+					.addValue("task_code", code);
 			Map<String, Object> result = call.execute(paramSource);
 			if (!result.isEmpty()) {
-				String rspCode = result.get("RESPONSE").toString();
+				String rspCode = result.get("return").toString();
 				if (rspCode.equals("00")) {
 					if (result.get("PAYMENTDETAILS") != null) {
-						rs = (ResultSet) result.get("PAYMENTDETAILS");
-						while (rs.next()) {
-							model = new SweepPaymentDetails();
-							model.setAc_branch(rs.getString("AC_BRANCH"));
-							model.setAc_no(rs.getString("AC_NO"));
-							model.setAc_ccy(rs.getString("AC_CCY"));
-							model.setTrn_code(rs.getString("TRN_CODE"));
-							model.setLcy_amount(rs.getString("AMOUNT"));
-							model.setCust_gl(rs.getString("CUST_GL"));
-							model.setExternal_ref_no(rs.getString("EXTERNAL_REF_NO"));
-							model.setDrcr_ind(rs.getString("DRCR_IND"));
-							model.setFormmnumber(rs.getString("FORMMNUMBER"));
-							model.setTask_code(code);
-							
-							models.add(model);
+						ArrayList<?> list = (ArrayList<?>) result.get("PAYMENTDETAILS");
+						JSONArray rsArray = new JSONArray(list);
+						for (int i = 0; i < rsArray.length(); i++) {
+							JSONObject rs = rsArray.getJSONObject(i);
+							if (rs != null && rs.toString().startsWith("{")) {
+								model = new SweepPaymentDetails();
+								model.setAc_branch(rs.getString("AC_BRANCH"));
+								model.setAc_no(rs.getString("AC_NO"));
+								model.setAc_ccy(rs.getString("AC_CCY"));
+								model.setTrn_code(rs.getString("TRN_CODE"));
+								model.setLcy_amount(rs.getBigDecimal("AMOUNT").toString());
+								model.setCust_gl(rs.getString("CUST_GL"));
+								model.setExternal_ref_no(rs.getString("EXTERNAL_REF_NO"));
+								model.setDrcr_ind(rs.getString("DRCR_IND"));
+								model.setFormmnumber(rs.optString("FORMMNUMBER"));
+								model.setTask_code(code);
+
+								models.add(model);
+							}
 						}
 						log.info("Successfully found sweep payment details");
 					} else {
@@ -399,36 +398,30 @@ public class PaymentDetailsRepository {
 			SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate);
 			call.setSchemaName(SCHEMANAME);
 			call.setCatalogName(PACKAGENAME);
-			call.setProcedureName("PERSISTSWEEPDATA");
-			call.declareParameters(new SqlOutParameter("respcode", Types.VARCHAR));
-			Map<String, Object> paramSource = new HashMap<>();
-			paramSource.put("BATCH_ID", sweepAgent.getBatchid());
-			paramSource.put("INITIATING_BRANCH", sweepAgent.getInitiatingbranch());
-			paramSource.put("REQUEST_MODULE", sweepAgent.getRequestmodule());
-			paramSource.put("MODULE_CREDENTIALS", sweepAgent.getModulecredentials());
-			paramSource.put("CURRENCY", sweepAgent.getTransactioncurrency());
-			paramSource.put("DEBITACC", sweepAgent.getDebitacccountnumber());
-			paramSource.put("CREDITACC", sweepAgent.getCreditacccountnumber());
-			paramSource.put("BRANCH_CODE", sweepAgent.getBranchcode());
-			paramSource.put("SRAMOUNT", sweepAgent.getAmount());
-			paramSource.put("DEBITCREDITIND", sweepAgent.getDebitcreditindicator());
-			paramSource.put("GLCASAIND", sweepAgent.getGlcasaindicator());
-			paramSource.put("NEMONIC", sweepAgent.getMnemonic());
-			paramSource.put("NARATION", sweepAgent.getNarration());
-			paramSource.put("NCSREFERENCE", sweepAgent.getNcscustomreference());
-			paramSource.put("PAYMENT_REFERENCE", sweepAgent.getPaymentreference());
-			paramSource.put("TRANS_ID", sweepAgent.getTransactionid());
-			paramSource.put("RESP_CODE", sweepAgent.getResponsecode());
-			paramSource.put("RESP_MESG", sweepAgent.getResponsemessage());
-			paramSource.put("RESPONSE_TIME", sweepAgent.getResponsetime());
-			paramSource.put("UIDSS", sweepAgent.getUids());
-			paramSource.put("BATCHDETAILID", sweepAgent.getBatch_detail_id());
-			paramSource.put("PAYMENTTYPEID", sweepAgent.getPayment_type_id());
-			paramSource.put("SWEEPSTATUS", sweepAgent.getIssweeporreversal());
-			paramSource.put("NCSSTATUS", sweepAgent.getNcstransstatus());
+			call.withFunctionName("PERSISTSWEEPDATA");
+			SqlParameterSource paramSource = new MapSqlParameterSource().addValue("BATCH_ID", sweepAgent.getBatchid())
+					.addValue("INITIATING_BRANCH", sweepAgent.getInitiatingbranch())
+					.addValue("REQUEST_MODULE", sweepAgent.getRequestmodule())
+					.addValue("MODULE_CREDENTIALS", sweepAgent.getModulecredentials())
+					.addValue("CURRENCY", sweepAgent.getTransactioncurrency())
+					.addValue("ACCOUNT", sweepAgent.getAcccountnumber())
+					.addValue("BRANCH_CODE", sweepAgent.getBranchcode()).addValue("SRAMOUNT", sweepAgent.getAmount())
+					.addValue("DEBITCREDITIND", sweepAgent.getDebitcreditindicator())
+					.addValue("GLCASAIND", sweepAgent.getGlcasaindicator())
+					.addValue("NEMONIC", sweepAgent.getMnemonic()).addValue("NARATION", sweepAgent.getNarration())
+					.addValue("NCSREFERENCE", sweepAgent.getNcscustomreference())
+					.addValue("PAYMENT_REFERENCE", sweepAgent.getPaymentreference())
+					.addValue("TRANS_ID", sweepAgent.getTransactionid())
+					.addValue("RESP_CODE", sweepAgent.getResponsecode())
+					.addValue("RESP_MESG", sweepAgent.getResponsemessage())
+					.addValue("RESPONSE_TIME", sweepAgent.getResponsetime()).addValue("UIDSS", sweepAgent.getUids())
+					.addValue("BATCHDETAILID", sweepAgent.getBatch_detail_id())
+					.addValue("PAYMENTTYPEID", sweepAgent.getPayment_type_id())
+					.addValue("SWEEPSTATUS", sweepAgent.getIssweeporreversal())
+					.addValue("NCSSTATUS", sweepAgent.getNcstransstatus());
 			Map<String, Object> respValues = call.execute(paramSource);
 			if (!respValues.isEmpty()) {
-				String responsecode = respValues.get("p_responsecode").toString();
+				String responsecode = respValues.get("return").toString();
 				if (responsecode.equals("00")) {
 					log.info("Successfully set the payment details with declarant code: "
 							+ sweepAgent.getNcscustomreference() + " as acknowledge");
